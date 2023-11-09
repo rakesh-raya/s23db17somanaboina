7dcf2b54e2600a7e97b8004a89cd7d1f533be7ce
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-08 18:34:27 -0600

    added logic to add the object to mongoDB

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -28,15 +28,37 @@ res.send(`{"error": ${err}}`);
 }
 };
 
+// Handle Restaurant create on POST.
+exports.restaurant_create_post = async function(req, res) {
+console.log(req.body)
+let document = new Restaurant();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"name":"Meridian", "cuisine": "Russian", "rating": 4.6}
+document.name = req.body.name;
+document.cuisine = req.body.cuisine;
+document.rating = req.body.rating;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
+
     
 // for a specific Restaurant.
 exports.restaurant_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Restaurant detail: ' + req.params.id);
 };
 // Handle Restaurant create on POST.
-exports.restaurant_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Restaurant create POST');
-};
+// exports.restaurant_create_post = function(req, res) {
+// res.send('NOT IMPLEMENTED: Restaurant create POST');
+// };
 // Handle Restaurant delete form on DELETE.
 exports.restaurant_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Restaurant delete DELETE ' + req.params.id);
