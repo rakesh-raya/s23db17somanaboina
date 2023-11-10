5a0ea6be6cfacfacef2aed2d823bcc585b982526
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-10 02:10:59 -0600

    added logic to get a single object from mongodb

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -52,9 +52,21 @@ res.send(`{"error": ${err}}`);
 
     
 // for a specific Restaurant.
-exports.restaurant_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Restaurant detail: ' + req.params.id);
+// exports.restaurant_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: Restaurant detail: ' + req.params.id);
+// };
+// for a specific Costume.
+exports.restaurant_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Restaurant.findById(req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
+
 // Handle Restaurant create on POST.
 // exports.restaurant_create_post = function(req, res) {
 // res.send('NOT IMPLEMENTED: Restaurant create POST');
