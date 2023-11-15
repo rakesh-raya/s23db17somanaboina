e192977510de82eb98a6760101ab1fd96f495504
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-14 18:09:47 -0600

    Added the logic to create through pug view

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -42,6 +42,22 @@ exports.restaurant_view_one_Page = async function(req, res) {
     }
     };
 
+// Handle building the view for creating a restaurant.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.restaurant_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('restaurantcreate', { title: 'Restaurant Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+
+
 // Handle Restaurant create on POST.
 exports.restaurant_create_post = async function(req, res) {
 console.log(req.body)
