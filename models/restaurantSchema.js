0da2f773eaf3749531b2fda089fedd27a8654116
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-20 23:27:03 -0600

    added buttons to create, detail and delete views

--- models/restaurantSchema.js
+++ models/restaurantSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const restaurantSchema = mongoose.Schema({
 name: String,
 cuisine: String,
-rating: Number
+rating: {type: Number, min: 0, max: 5}
 })
 module.exports = mongoose.model("Restaurant",
 restaurantSchema)
