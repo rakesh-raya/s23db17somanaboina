431dafe0935f48a0ba4595bb31d33b25da5e3835
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-20 23:03:36 -0600

    Added authentication logic for restaurant app

--- app.js
+++ app.js
@@ -2,7 +2,30 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+  
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -14,39 +37,43 @@ mongoose.connect(connectionString)
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 var Restaurant = require("./models/restaurantSchema");
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
-// Delete everything
-await Restaurant.deleteMany();
-let instance1 = new
-Restaurant({name:"Marriot", cuisine:'Indian', rating:4.9});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance2 = new
-Restaurant({name:"RedFox", cuisine:'Italian', rating:4.1});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
-let instance3 = new
-Restaurant({name:"Trident", cuisine:'Mexican', rating:4.5});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
+async function recreateDB() {
+  // Delete everything
+  await Restaurant.deleteMany();
+  let instance1 = new
+    Restaurant({ name: "Marriot", cuisine: 'Indian', rating: 4.9 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+  let instance2 = new
+    Restaurant({ name: "RedFox", cuisine: 'Italian', rating: 4.1 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+  let instance3 = new
+    Restaurant({ name: "Trident", cuisine: 'Mexican', rating: 4.5 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -65,6 +92,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -74,13 +108,21 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,7 +1,12 @@
 var express = require('express');
 const restaurant_controllers= require('../controllers/restaurants');
 var router = express.Router();
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('restaurant', { title: 'Search Results Restaurant' });
@@ -14,7 +19,7 @@ router.get('/detail', restaurant_controllers.restaurant_view_one_Page);
 /* GET create restaurant page */
 router.get('/create', restaurant_controllers.restaurant_create_Page);
 /* GET create update page */
-router.get('/update', restaurant_controllers.restaurant_update_Page);
+router.get('/update',secured, restaurant_controllers.restaurant_update_Page);
 /* GET delete restaurant page */
 router.get('/delete', restaurant_controllers.restaurant_delete_Page);
 
