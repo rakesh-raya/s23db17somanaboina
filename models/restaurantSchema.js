56505053d59fbba5929fad37abe646982ba4b0d4
rakesh-raya <rakesh.raya9696@gmail.com>
2023-11-29 17:19:53 -0600

    added the endpoints protection

--- models/restaurantSchema.js
+++ models/restaurantSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const restaurantSchema = mongoose.Schema({
 name: String,
-cuisine: String,
+cuisine: {type: String,  match: /^[^\d]+$/},
 rating: {type: Number, min: 0, max: 5}
 })
 module.exports = mongoose.model("Restaurant",
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -17,11 +17,11 @@ router.get('/', restaurant_controllers.restaurant_view_all_Page );
 /* GET detail restaurant page */
 router.get('/detail', restaurant_controllers.restaurant_view_one_Page);
 /* GET create restaurant page */
-router.get('/create', restaurant_controllers.restaurant_create_Page);
+router.get('/create',secured, restaurant_controllers.restaurant_create_Page);
 /* GET create update page */
 router.get('/update',secured, restaurant_controllers.restaurant_update_Page);
 /* GET delete restaurant page */
-router.get('/delete', restaurant_controllers.restaurant_delete_Page);
+router.get('/delete',secured, restaurant_controllers.restaurant_delete_Page);
 
 
 module.exports = router;
